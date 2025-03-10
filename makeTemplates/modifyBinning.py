cdbba0d4ebdb352b92b0195c0bf9f7b2ecccc0de
xshen <xiaohe_shen@brown.edu>
2025-03-10 16:51:47 -0500

    Normalize signal to theory xsec

--- makeTemplates/modifyBinning.py
+++ makeTemplates/modifyBinning.py
@@ -52,6 +52,20 @@ if 'kinematics' in folder:
 	normalizePDF    = False #only for signals
 
 massList = [800,1000,1200,1300,1400,1500,1600,1700,1800,2000,2200]
+theory_xsec = {"BpM800":0.1187124,
+               "BpM900":0.0640113,
+               "BpM1000":0.0362987,
+               "BpM1100":0.0215009,
+               "BpM1200":0.0131348,
+               "BpM1300":0.0082629,
+               "BpM1400":0.0053213,
+               "BpM1500":0.0035078,
+               "BpM1600":0.0022829,
+               "BpM1700":0.0014947,
+               "BpM1800":0.0009898,
+               "BpM1900":0.0006519,
+               "BpM2000":0.0004499,
+               "BpM2200":1.0} # no theory xsec for 2200
 sigProcList = ['BpM'+str(mass) for mass in massList]
 bkgProcList = ['ttbar','singletop','wjets','ttx','ewk','qcd'] #put the most dominant process first
 if 'ABCDnn' in iPlot:
@@ -303,7 +317,7 @@ for rfile in rfiles:
                                 rebinnedHists[hist] = tfiles[iRfile].Get(hist).Rebin(len(xbins[chn])-1,hist,xbins[chn])
                         rebinnedHists[hist].SetDirectory(0)
                         if '__'+sigName in hist:
-                                rebinnedHists[hist].Scale(1.0/0.5) # already did lumi*1pb/Ngen, need lumi*1pb/(Ngen*BRsinglet)
+                                rebinnedHists[hist].Scale(theory_xsec[hist.split('__')[1]]/0.5) # already did lumi*1pb/Ngen, need lumi*1pb/(Ngen*BRsinglet) # then scale to theory xsec
                         if rebinnedHists[hist].Integral() < 1e-12: 
                                 print("Empty hist found, skipping: "+hist)
                                 continue
